abc4f05c172e3e64220013dcce369d233dace96c
Tjark Sievers <tjarksievers@icloud.com>
2024-07-15 16:35:04 +0200

    Problem 6 solved

--- 6_Sum_Square_Difference.py
+++ 6_Sum_Square_Difference.py
@@ -7,7 +7,7 @@ The sum of the squares of the first ten natural numbers is
 
 The square of the sum of the first ten natural numbers is
 
-    (1^2 + 2^2 + ... + 10)^2 = 55^2 = 3025.
+    (1 + 2 + ... + 10)^2 = 55^2 = 3025.
 
 Hence, the difference between the sum of the squares of the first ten natural numbers and the square of the sum is
 
@@ -19,5 +19,14 @@ https://projecteuler.net/problem=6
 """
 
 
+def sum_square_difference(n: int):
+    return 0.25 * n**4 + n**3 / 6 - 0.25 * n**2 - n / 6
+
+
 if __name__ == "__main__":
-    pass
+    print(
+        f"For checking: difference of sum of squares and square of sum for n = 10: {int(sum_square_difference(10))}"
+    )
+    print(
+        f"For checking: difference of sum of squares and square of sum for n = 100: {int(sum_square_difference(100))}"
+    )
